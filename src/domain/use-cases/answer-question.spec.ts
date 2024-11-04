8b49e602fcb660caa09f153cd525e717d9c0dfd1
José Victor <josevictornascimento2016@gmail.com>
2024-11-04 19:55:15 -0300

    feat: configure eslint

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -3,9 +3,8 @@ import { AnswersRepository } from "../repositories/answers-repository";
 import { Answer } from "../entities/answer";
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create: async (answer: Answer) => {},
 };
 test("create an answer", async () => {
   const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
