cbacfcdf4be1eac9a8e1b35c7ddf4ad37d72438f
José Victor <josevictornascimento2016@gmail.com>
2024-11-05 20:32:00 -0300

    feat: generate dummy data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,16 +3,22 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example question',
-    slug: Slug.create('example-question'),
-    authorId: new UniqueEntityID(),
-    content: 'Example content',
-    ...override,
-  })
+import { faker } from '@faker-js/faker'
+
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
