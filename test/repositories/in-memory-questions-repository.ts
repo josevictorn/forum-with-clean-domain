28e1aefd3349d1570bccd4eb6cdaac45b5bf6b06
José Victor <josevictornascimento2016@gmail.com>
2024-11-06 22:04:58 -0300

    feat: delete a question

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,8 +4,14 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
-  async create(question: Question): Promise<void> {
-    this.items.push(question)
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
   }
 
   async findBySlug(slug: string): Promise<Question | null> {
@@ -16,5 +22,15 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question
+  }
+
+  async create(question: Question): Promise<void> {
+    this.items.push(question)
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
